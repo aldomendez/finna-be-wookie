9c112f0b42f31de6ffe8a9f532ad7f588e1301f9
Aldo Mendez <aldomendez86@gmail.com>
2015-12-14 19:06:23 -0600

    now is working

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Captura de material indirecto</title>
+  <link rel="stylesheet" type="text/css"  href="../jsLib/SemanticUi/1.0.0/packaged/css/semantic.min.css">
+<!--  <script src="../jsLib/SemanticUI/0.19.3/javascript/semantic.js"></script>-->
+  <link rel="stylesheet" type="text/css" href="../jsLib/nprogress/nprogress.css">
+</head>
+<body class="">
+  <div class="ui padded grid">
+    <div class="column"  id="container"></div>
+  </div>
+  <script type="template/ractive" id="template"><?php include 'home.template.php' ?></script>
+  <!-- // <script src="../jsLib/jquery/jquery-2.1.1.min.js"></script> -->
+  <!-- <script src="../jsLib/nprogress/nprogress.js"></script> -->
+  <!-- // <script src="../jsLib/fuse.js/fuse.js"></script> -->
+  <!-- // <script src="../jsLib/underscore/1.6.0/underscore.js"></script>  -->
+  <script src="../jsLib/ractivejs/0.7.1/ractive.min.js"></script>
+  <!-- // <script src="../jsLib/ractive-events/0.1.1/mousewheel.js"></script> -->
+  <script type="text/javascript" src="home.js"></script>
+</body>
+</html>
